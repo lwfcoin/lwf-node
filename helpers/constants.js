bd7f9c80d8c5e5a0a2fbaaadcf92bf4d2830be25
Stefano Gessa <stefano@gessa.net>
2018-06-22 17:16:51 +0700

    raise maxPeers to 200

--- helpers/constants.js
+++ helpers/constants.js
@@ -35,7 +35,7 @@ module.exports = {
 	maxAmount: 100000000,
 	maxConfirmations: 77 * 100,
 	maxPayloadLength: 1024 * 1024,
-	maxPeers: 100,
+	maxPeers: 200,
 	maxRequests: 10000 * 12,
 	maxSharedTxs: 100,
 	maxSignaturesLength: 196 * 256,
