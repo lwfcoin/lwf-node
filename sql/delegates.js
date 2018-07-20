235f255ae14f7099bc8d4f848af129b3fb963085
Carlos Trancoso Fernandez <ctrancoso.b9@gmail.com>
2018-07-20 21:13:30 +0200

    added full delegates API

--- sql/delegates.js
+++ sql/delegates.js
@@ -48,6 +48,50 @@ var DelegatesSql = {
     return pgp.as.format(sql, params);
   },
 
+  getForgers: function (params) {
+    var sql = [
+      'WITH',
+      'supply AS (SELECT calcSupply((SELECT height FROM blocks ORDER BY height DESC LIMIT 1))::numeric),',
+      'delegates AS (SELECT row_number() OVER (ORDER BY vote DESC, m."publicKey" ASC)::int AS rank,',
+        'm.username,',
+        'm.address,',
+        'ENCODE(m."publicKey", \'hex\') AS "publicKey",',
+        'm.vote,',
+        'm.producedblocks,',
+        'm.missedblocks,',
+        'ROUND(vote / (SELECT * FROM supply) * 100, 2)::float AS approval,',
+        '(CASE WHEN producedblocks + missedblocks = 0 THEN 0.00 ELSE',
+          'ROUND(100 - (missedblocks::numeric / (producedblocks + missedblocks) * 100), 2)',
+        'END)::float AS productivity,',
+        'COALESCE(v.voters_cnt, 0) AS voters_cnt,',
+        't.timestamp AS register_timestamp',
+      'FROM delegates d',
+      'LEFT JOIN mem_accounts m ON d.username = m.username',
+      'LEFT JOIN trs t ON d."transactionId" = t.id',
+      'LEFT JOIN (SELECT "dependentId", COUNT(1)::int AS voters_cnt from mem_accounts2delegates GROUP BY "dependentId") v ON v."dependentId" = ENCODE(m."publicKey", \'hex\')',
+      'WHERE m."isDelegate" = 1',
+      'ORDER BY ' + [params.sortField, params.sortMethod].join(' ') + ')',
+      'SELECT * FROM delegates LIMIT ${limit} OFFSET ${offset}'
+    ].join(' ');
+
+    return pgp.as.format(sql, params);
+  },
+
+  // getLastBlockByGeneratorPublicKey: function() {
+  //   var sql = [
+  //     'WITH',
+  //     'lastBlocks AS (SELECT row_number() OVER (ORDER BY b."height" DESC)::int AS rn,',
+  //       'b.*',
+  //     'FROM blocks b',
+  //     'WHERE b."generatorPublicKey" = DECODE(${generatorPublicKey}, \'hex\'))',
+  //     'SELECT * FROM lastBlocks WHERE lastBlocks."rn" = 1'
+  //   ].join(' ');
+    
+  //   return sql;
+  // },
+
+  getLastBlockByGeneratorPublicKey:  'SELECT * FROM blocks b WHERE b."generatorPublicKey" = DECODE(${generatorPublicKey}, \'hex\') ORDER BY b."height" DESC LIMIT 1;',
+
   insertFork: 'INSERT INTO forks_stat ("delegatePublicKey", "blockTimestamp", "blockId", "blockHeight", "previousBlock", "cause") VALUES (${delegatePublicKey}, ${blockTimestamp}, ${blockId}, ${blockHeight}, ${previousBlock}, ${cause});',
 
   getVoters: 'SELECT ARRAY_AGG("accountId") AS "accountIds" FROM mem_accounts2delegates WHERE "dependentId" = ${publicKey}'
